71220f54432ef6b1260e6c65357b4e885cffca70
Allan <allancheboiw0@gmail.com>
2022-08-22 07:41:31 +0300

    Add game function

--- Foundation/Projects/rock-paper-scissors/script.js
+++ Foundation/Projects/rock-paper-scissors/script.js
@@ -1,3 +1,6 @@
+let playerScore = 0;
+let computerScore = 0;
+
 function getComputerChoice(){
     const game_array = ["rock", "paper", "scissors"];
 
@@ -9,24 +12,38 @@ function playRound(playerSelection, computerSelection){
     const player_selection = playerSelection.toLowerCase();
 
     if (player_selection === "rock" && computerSelection === "paper") {
+        computerScore++;
         return "You Lose! paper beats rock";
     } else if (player_selection === "rock" && computerSelection === "scissors") {
+        playerScore++;
         return "You Win! rock beats scissors";
     } else if (player_selection === "scissors" && computerSelection === "paper") {
+        playerScore++;
         return "You Win! scissors beats paper";
     } else if (player_selection === "paper" && computerSelection === "rock") {
+        playerScore++;
         return "You Win! paper beats rock";
     } else if (player_selection === "scissors" && computerSelection === "rock") {
+        computerScore++;
         return "You Lose! rock beats scissors";
     } else if (player_selection === "paper" && computerSelection === "scissors") {
+        computerScore++;
         return "You Lose! scissors beats paper";
     } else {
         return "It's a draw."
     }
 }
 
-const playerSelection = "rock";
-console.log(`Player: ${playerSelection}`);
-const computerSelection = getComputerChoice();
-console.log(`Computer: ${computerSelection}`);
-console.log(playRound(playerSelection, computerSelection));
+
+function game() {
+    for (let rounds = 0; rounds < 5; rounds++) {
+        const playerSelection = prompt("Choose your weapon.");
+        console.log(`Player: ${playerSelection}`);
+        const computerSelection = getComputerChoice();
+        console.log(`Computer: ${computerSelection}`);
+        console.log(playRound(playerSelection, computerSelection));
+        console.log(`Player Score: ${playerScore}\nComputer Score: ${computerScore}`);
+    }
+}
+
+game();
